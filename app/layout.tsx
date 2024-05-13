29ada83bdada02f6a092658fd0a3067fc3661372
Kevin Hang <kevinhang.kh@gmail.com>
2024-05-13 12:25:56 +0200

    Create app page and layout files

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import '@/styles/globals.css';
+import { ReactNode } from 'react';
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
+};
+
+function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
