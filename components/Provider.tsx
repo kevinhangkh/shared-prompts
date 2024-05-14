c0d5db623dba71ee9485bb92e8460fee3628e189
Kevin Hang <kevinhang.kh@gmail.com>
2024-05-14 16:20:13 +0200

    Make session and children optional in Provider

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -4,8 +4,8 @@ import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 
 interface ProviderProps {
-  session: any;
-  children: ReactNode;
+  session?: any;
+  children?: ReactNode;
 }
 
 function Provider({ session, children }: ProviderProps) {
