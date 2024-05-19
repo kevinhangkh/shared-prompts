2a97ec70a8b88b18c2a92c898f4a5007285a9abb
Kevin Hang <kevinhang.kh@gmail.com>
2024-05-19 15:33:17 +0200

    Add edit and delete icon

--- components/feed/PromptCard.tsx
+++ components/feed/PromptCard.tsx
@@ -60,18 +60,26 @@ function PromptCard({
 
       {postBelongsToCurrentUserAndProfilePage() && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
-          <p
-            className="font-inter text-sm green_gradient cursor-pointer"
-            onClick={handleEdit}
-          >
-            Edit
-          </p>
-          <p
-            className="font-inter text-sm orange_gradient cursor-pointer"
-            onClick={handleDelete}
-          >
-            Delete
-          </p>
+          <div className="copy_btn">
+            <Image
+              src="/assets/icons/edit.svg"
+              alt="edit"
+              width={15}
+              height={15}
+              className="cursor-pointer"
+              onClick={() => handleEdit && handleEdit(post)}
+            />
+          </div>
+          <div className="copy_btn">
+            <Image
+              src="/assets/icons/trash.svg"
+              alt="trash"
+              width={18}
+              height={18}
+              className="cursor-pointer"
+              onClick={() => handleDelete && handleDelete(post)}
+            />
+          </div>
         </div>
       )}
     </div>
